8693eb52d96d3100641ba18c79f06f93cddce231
Patrick Clary <pclary@outlook.com>
2015-03-12 14:10:12 -0700

    Fixed sampling function for center grids

--- sampledist.m
+++ sampledist.m
@@ -7,7 +7,11 @@ function [xp, yp] = sampledist(dist, n, x, y, sampfun)
 % Updated 12/19/2014
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Sample using the supplied sample function or the default halton sequence;
+
+% Ensure that the distribution has no negative values
+dist(dist < 0) = 0;
+
+% Sample using the supplied sampling function or the default halton sequence;
 % Samples are points in the domain [0, 1] x [0, 1]
 if nargin < 5
     s1 = halton(2, n);
@@ -24,24 +28,39 @@ if nargin < 4 || (isempty(x) && isempty(y))
 end
 if numel(x) == 2 && numel(y) == 2 && any(size(dist) ~= [2, 2])
     % Treat x and y as bounds, not grid vectors
-    x = linspace(x(1), x(2), size(dist, 2));
-    y = linspace(y(1), y(2), size(dist, 1));
+    % Create center grid
+    dx = diff(x)/size(dist, 2);
+    dy = diff(y)/size(dist, 1);
+    x = x(1) - dx/2:dx:x(2) + dx/2;
+    y = y(1) - dy/2:dy:y(2) + dy/2;
+else
+    dx = diff(x(1:2));
+    dy = diff(y(1:2));
 end
     
 % Construct an approximate 2D cumulative probability density function
 % respresenting the distribution
-w = cumtrapz(trapz(dist, 2));
+weights = trapz(dist, 2);
+w = cumtrapz(weights);
+w = w + weights(1) / 2;
 if w(end) ~= 0
-    w = w/w(end);
+    w = w/(w(end) + weights(end)/2);
 else
-    w = linspace(0, 1, numel(w))';
+    l1 = numel(q);
+    w = linspace(1/l1/2, 1 - 1/l1/2, l1)';
 end
+weights = [0; weights; 0];
+w = [-w(1); w; 2 - w(end)];
 ww = cumtrapz(dist, 2);
-ww = bsxfun(@rdivide, ww, ww(:, end));
+ww = bsxfun(@plus, ww, dist(:, 1) / 2);
+ww = bsxfun(@rdivide, ww, ww(:, end) + dist(:, end)/2);
 index = find(isnan(ww(:, end)));
+l2 = size(ww, 2);
 for i = index'
-    ww(i, :) = linspace(0, 1, size(ww, 2));
+    ww(i, :) = linspace(1/l2/2, 1 - 1/l2/2, l2);
 end
+ww = [linspace(1/l2/2, 1 - 1/l2/2, l2); ww; linspace(1/l2/2, 1 - 1/l2/2, l2)];
+ww = [-ww(:, 1), ww, 2 - ww(:, end)];
 
 xp = zeros(n, 1);
 yp = zeros(n, 1);
@@ -52,12 +71,11 @@ for k = 1:n
     i = find(w > s1(k), 1);
     f = (s1(k)-w(i-1))/(w(i)-w(i-1));
     yp(k) = y(i-1) + (y(i)-y(i-1))*f;
-    wwi = ww(i-1,:) + (ww(i,:) - ww(i-1,:))*f;
-    wwi = wwi/wwi(end);
-    i = find(wwi > s2(k), 1);
-    f = (s2(k)-wwi(i-1))/(wwi(i)-wwi(i-1));
-    xp(k) = x(i-1) + (x(i)-x(i-1))*f;
+    wwi = ww(i-1,:)*weights(i-1) + (ww(i,:)*weights(i) - ww(i-1,:)*weights(i-1))*f;
+    wwi2 = wwi / wwi(end);
+    i2 = find(wwi2 > s2(k), 1);
+    f = (s2(k)-wwi2(i2-1))/(wwi2(i2)-wwi2(i2-1));
+    xp(k) = x(i2-1) + (x(i2)-x(i2-1))*f;
 end
 
 end
-
